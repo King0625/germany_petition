304b7390c88e4f33964941024f58d0eda8b4a531
King0625 <a7164265@gmail.com>
2019-10-03 16:55:37 +0800

    finish the success and fail judgement

--- petition_count.php
+++ petition_count.php
@@ -1,5 +1,5 @@
 <?php
-// date_default_timezone_set('Asia/Taipei');
+date_default_timezone_set('Asia/Taipei');
 include('config.php');
 
 $ch = curl_init(URL);
@@ -18,3 +18,17 @@ $nodeList = $xpath->query('//span[@class="mzanzahl"]');
 $petition_count = $nodeList[0]->nodeValue;
 var_dump(trim($petition_count));
 
+// $form_url = "https://epetitionen.bundestag.de/content/epet/registrieren.html";
+// $ch = curl_init($form_url);
+// curl_setopt($ch, CURLOPT_HTTPGET, true);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array(COOKIE));
+// $result = curl_exec($ch);
+
+// $dom = new DOMDocument;
+// @$dom->loadHTML($result);
+// $dom->saveHTML($dom->documentElement);
+
+// $ol = $dom->getElementsByTagName('ol');
+
+// var_dump($ol);
