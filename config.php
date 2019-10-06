3e44b0f68972b6f1d2e182d9919e3db470546191
King0625 <a7164265@gmail.com>
2019-10-06 19:35:56 +0800

    Add cors in config.php

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Taipei');
+header("Content-type:application/json");
+header("Access-Control-Allow-Origin: *");
 
 define('COOKIE', 'Cookie: renderid=s292; JSESSIONID=1whn24j1xm6ts38mii9ul6281; SERVERID=ba54139d16c4262a64bd199a74bd72063a5d8bcf');
